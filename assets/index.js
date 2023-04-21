f5cf366a08a46ad33598c79e09142c19bf3ad089
zulfuqar2 <anarzulfuqar2005@gmail.com>
2023-04-21 11:33:52 +0400

    yaradildi

---
+++ assets/index.js
@@ -0,0 +1,22 @@
+class Students{
+    constructor(fullname,id){
+        this.fullname =fullname;
+        this.id =id;
+    }updateFullname(name){
+        this.fullname.push(name);
+    }
+
+}
+
+
+class Group{
+
+}
+
+
+class Faculity{
+
+}
+
+const Students2 =new Students("zulfuqar seyfullali",7658766);
+console.log(Students2);
